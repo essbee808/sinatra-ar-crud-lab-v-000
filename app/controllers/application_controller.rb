4250a3eaf9dc47a543c18263dc952d05b9247d7d
Sheena Bonilla <sheena.myb@gmail.com>
2019-04-12 16:48:54 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -42,7 +42,7 @@ class ApplicationController < Sinatra::Base
   end
 
   delete '/articles/:id' do
-    @articles = Articles.all
+    @articles = Article.all
     @articles.select do |el|
        binding.pry
       if el.id == params[:id]
